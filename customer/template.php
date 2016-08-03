7bb298e9952de70a5d08d71dafa36fd4fe847e7b
kalpasameera <kalpasameeramadanayake@gmail.com>
2016-08-03 15:31:50 +0530

    Kalpa
    Home

--- customer/template.php
+++ customer/template.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Theme</title>
-    <link rel="stylesheet" href="designs/template.css" type="text/css" />
+    <link rel="stylesheet" href="main.css" type="text/css" />
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
