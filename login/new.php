fb410da505343b9ea5b8789eb6819509635e7014
Delushaan <delushaan@outlook.com>
2016-08-05 16:42:52 +0530

    Delushaan
    Developed login and signup

--- login/new.php
+++ login/new.php
@@ -1,10 +1,7 @@
-<html>
-<head>
 <?php
 include ("../config/database.php");
 ?>
-</head>
-<body>
+
 <div id="login">
     <div id="frm">
         <form method="post">
@@ -24,32 +21,31 @@ include ("../config/database.php");
                     <td colspan="2"><input type="submit" name="login" value="Login"></td>
                 </tr>
         </form>
-</body>
+
 <?php
-session_start();
-if(isset($_POST['login']))
-{
-    $name=$_POST['username'];
-    $pwd=$_POST['password'];
-    if($name!=''&&$pwd!='')
-    {
-        $sql = "select * from customer where username='".$name."' and password='".$pwd."'";
-        $query=mysqli_query($conn,$sql) or die("Not user found");
-        $res=mysqli_fetch_row($query);
-        if($res)
-        {
-            $_SESSION['name']=$name;
-            header('location:index.php');
-        }
-        else
-        {
-            echo'You entered username or password is incorrect';
-        }
-    }
-    else
-    {
-        echo'Enter both username and password';
-    }
+$userName=$_POST['username'];
+$password=$_POST['password'];
+$passWord=md5($password); // Encrypted Password
+
+//*********retrieving data from Database**********
+
+$query = "SELECT * FROM cutomer WHERE username='$userName' and password='$passWord'";
+//$login = mysqli_query("SELECT userName,password FROM 'member' WHERE userName=          $_post['username'] AND passWord= $_post['password'])");
+// Check username and password match
+$res = mysql_query($query);
+
+$rows = mysqli_num_rows($res);
+if ($rows==1) {
+    // Set username session variable
+
+    $_SESSION['userName'] = $_POST['username'];
+
+    // Jump to secured page
+    header("Location: index.php");
 }
+else {
+    // Jump to login page
+    echo "user name and password not found";
+}
+exit;
 ?>
-</html>
