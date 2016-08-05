700201c2a556915855269a8bedd295bed96d221f
madhara <madharaudagedara@gmail.com>
2016-08-05 09:37:58 +0530

    Madhara
    Manage customer

---
+++ manager/Customer-Manage_Customer.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Theme</title>
+    <link rel="stylesheet" href="designs/template.css" type="text/css" />
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="designs/template.css" type="text/css" />
+    <link rel="stylesheet" href="../config/design/css/theme.css" type="text/css">
+    <?php
+        include ("../config/headermanager.html");
+    ?>
+</head>
+<body>
+</br></br>
+<div id ="lol"</div>
+<div id="main">
+<div id="header">
+        <img src="images/Managing.jpg">
+    </div>
+
+<div id="menu">
+  			<h2>
+            	<font color="white" size="+1">Manage Customer</font>
+            </h2>
+    </div>
+
+    <div id="content">
+		<div id="top">
+        	<div id="top-left">
+				<form method="post">
+					<table border="0">
+						<tr></tr>
+						<tr>
+							<td id="table-font" width="60%">
+								First Name
+							</td>
+							<td>
+								<input type="text" name="First Name" class="form-control">
+								<br><br>
+							</td>
+						</tr>
+
+						<tr>
+							<td id="table-font" width="60%">
+								Last Name
+							</td>
+							<td>
+								<input type="text" name="Last Name" class="form-control">
+								<br><br>
+							</td>
+						</tr>
+
+						<tr>
+							<td id="table-font" width="60%">
+								NIC Number
+							</td>
+							<td>
+								<input type="text" name="NIC Number" class="form-control">
+								<br><br>
+							</td>
+						</tr>
+
+						<tr>
+							<td id="table-font" width="60%">
+								Company
+							</td>
+							<td>
+								<input type="text" name="Company" class="form-control">
+								<br><br>
+							</td>
+						</tr>
+						
+						<button type="button" id="button1" class="btn btn-default btn-lg active">Approve</button>
+
+					</table>
+				</form>
+			</div>
+			<div id="top-right">
+			</div>
+		</div>
+		<div id="mid">
+			<div id="mid-left">
+
+			</div>
+			<div id="mid-right">
+
+			</div>
+		</div>
+		<div id="bottom">
+			<div id="bottom-left">
+
+			</div>
+			<div id="bottom-right">
+
+			</div>
+		</div>
+	</div>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+    </div>
+    <div id="footer"></div>
+</div>
+</body>
+</html>
