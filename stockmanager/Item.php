0b5e217ab3bdecfa457cce51d832c2e9709a8b46
UthpalaPitawela <uthpala.isiru@gmail.com>
2016-08-12 14:47:53 +0530

    Uthpala
    search button

--- stockmanager/Item.php
+++ stockmanager/Item.php
@@ -31,6 +31,29 @@
 
 	}
 
+	public function searchItem($itemname){
+		include('database_connection.php');
+		$sqlinsert = "SELECT * FROM item WHERE itemName == $itemname";
+
+		$result = mysqli_query($dbcon, $sqlinsert);
+
+		if($result->num_rows > 0){
+			 echo "<table><tr><th>Category Name</th><th>Item ID</th><th>Item Name</th><th>Brand</th><th>Cost</th><th>Selling Price</th><th>Quantity</th></tr>";
+			 while($row = $result->fetch_assoc()) {
+        		echo "<tr><td>".$row["id"]."</td><td>".$row["firstname"]." ".$row["lastname"]."</td></tr>";
+    		}
+    			echo "</table>";
+		} else {
+    			echo "0 results";
+
+		}
+
+
+
+
+
+	}
+
 }
 
 
