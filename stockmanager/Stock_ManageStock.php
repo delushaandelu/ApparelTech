518523d4fdf7472f7207a97d406b25820cef4d47
UthpalaPitawela <uthpala.isiru@gmail.com>
2016-08-12 12:45:00 +0530

    Uthpala
    create search button

--- stockmanager/Stock_ManageStock.php
+++ stockmanager/Stock_ManageStock.php
@@ -154,9 +154,11 @@
 									$quantity = $_POST['quantity'];
 									$myItem = new Item();
 									$myItem -> addItem($itemname, $brand,$sellingprice,$cost, $categoryname, $itemid,$quantity );
+								}
 									
+								if(isset($_POST['btnManageStockSearch'])){
 									
-								
+									$itemname = $_POST['itemname'];
 									
 									}
                             
