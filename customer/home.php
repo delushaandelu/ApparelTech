a6719b69e7528234e50566bfc72fb4d8b75617bc
Delushaan <delushaan@outlook.com>
2016-08-20 00:07:52 -0700

    Delushaan
    Customer profile path edited

--- customer/home.php
+++ customer/home.php
@@ -3,16 +3,16 @@
   <head>
     <meta charset="utf-8">
     <title>ApperalTech</title>
-    <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <link href="../build/css/custom.min.css" rel="stylesheet">
+    <link href="vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="build/css/custom.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/home.css" type="text/css">
   </head>
 
   <body class="nav-md">
     
   <?php
-    include("../../config/customermenu.php");
+    include("../config/customermenu.php");
   ?>
 
 <!-- page content -->
@@ -48,10 +48,10 @@
 </body>
 
 <!-- jQuery -->
-<script src="../vendors/jquery/dist/jquery.min.js"></script>
+<script src="vendors/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap -->
-<script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- FastClick -->
   <!-- Custom Theme Scripts -->
-<script src="../build/js/custom.min.js"></script>
+<script src="build/js/custom.min.js"></script>
 </html>
