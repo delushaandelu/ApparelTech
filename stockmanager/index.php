300aebb15a6692928d0e8a5db4bad4632502ff52
Delushaan <delushaan@outlook.com>
2016-09-05 19:15:18 +0530

    Delushaan
    Stockmanager temperory login has created

---
+++ stockmanager/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>ApparelTech</title>
+        <link rel="stylesheet" href="css/style.css">
+  </head>
+    <?php
+if(isset($_POST['login'])){
+    require ('../config/database.php');
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+   
+    $result = mysqli_query($conn, 'select * from user where username="'.$username.'" and password="'.$password.'"');
+    if (mysqli_num_rows($result)==1){
+        header('location: HomePage.php');
+    }else
+        echo "<script>";
+        echo "alert('ERROR| Your are not an authoriezed user! Cheack your information again')";
+        echo "</script>";
+}
+?>
+  <body>
+    <hgroup>
+  <h1>ApparelTech</h1>
+  <h3>Operational Level Login</h3>
+</hgroup>
+<form method="POST">
+  <div class="group">
+    <input type="text" name="username">
+    <label>Username</label>
+  </div>
+  <div class="group">
+    <input type="password" name="password">
+    <label>Password</label>
+  </div>
+  <button type="submit" class="button buttonBlue" name="login">Login
+    <div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
+  </button>
+</form>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src="js/index.js"></script>
+  </body>
+</html>
