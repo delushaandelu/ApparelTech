3e60aaf1e9d3d3ef8aa7163526637604d26f2a9e
madhara <madharaudagedara@gmail.com>
2016-09-08 22:35:01 +0530

    Madhara
    alert in manage customer

--- manager/ManageUser-Manage_Customer.php
+++ manager/ManageUser-Manage_Customer.php
@@ -7,6 +7,8 @@
     <link href="dist/css/sb-admin-2.css" rel="stylesheet">
     <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="designs/template.css" type="text/css" />
+    <script src="js/sweetalert-dev.js"></script>
+    <link rel="stylesheet" href="js/sweetalert.css">
 </head>
 
 <body>
@@ -61,34 +63,10 @@
                         <td><?php echo $row['mobile'] ?></td>
                         <td><?php echo $row['tele'] ?></td>
                 <td class="bt"><input type="button" id="button" class="btn btn-info" value="Accept" onclick="location.href='ManageUser-Manage_Customer.php?cst_id=<?php echo $row['customer_id'] ?>'"></td>
-                        <td class="bt"><input type="button" id="button" class="btn btn-info" value="Reject" data-toggle="modal" data-target="#myModal"  onclick="location.href='ManageUser-Manage_Customer.php?customer_id=<?php echo $row['customer_id'] ?>'"></td>
-                    
-
-                <div class="modal fade" id="myModal" role="dialog">
-                    <div class="modal-dialog">
-
-                        <!-- Modal content-->
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-
-                            </div>
-                            <div class="modal-body">
-                                <p>Customer Rejected</p>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-
-
-                    </tr>
+                        <td class="bt"><input type="button" id="button" class="btn btn-info" value="Reject" onclick="location.href='ManageUser-Manage_Customer.php?customer_id=<?php echo $row['customer_id'] ?>'"></td>
+            </tr>
         <?php } ?>
-    </table>";
+    </table>
 
 
 
@@ -101,14 +79,14 @@
 
         if($result){
             echo'<script language ="javascript">';
-            echo'alert("customer rejected succesfully")';
-            echo 'window.location ="\'ManageUser-Manage_Customer.php\'" ';
+            echo "swal({  title: 'Customer deleted successfully!', text: '', type: 'success', confirmButtonText: 'Done!'}, function(){window.location.href='ManageUser-Manage_Customer.php'});";
             echo'</script>';
             }
         else{
             echo'<script language ="javascript">';
-            echo'alert("Error")';
-            echo'</script>'; }
+            echo "swal({  title: 'Error occurs while deleting!', text: '', type: 'error', confirmButtonText: 'Done!'}, function(){window.location.href='ManageUser-Manage_Customer.php'});";
+            echo'</script>';
+        }
     }
 ?>
 
@@ -124,14 +102,14 @@
 
         if($result1){
             echo'<script language ="javascript">';
-            echo'alert("customer rejected succesfully")';
-            echo 'window.location ="\'ManageUser-Manage_Customer.php\'" ';
+            echo "swal({  title: 'Details of customer saved successfully!', text: '', type: 'success', confirmButtonText: 'Done!'}, function(){window.location.href='ManageUser-Manage_Customer.php'});";
             echo'</script>';
         }
         else{
             echo'<script language ="javascript">';
-            echo'alert("This is your exsisting customer!")';
-            echo'</script>'; }
+            echo "swal({  title: 'Error occurs while saving data!', text: '', type: 'error', confirmButtonText: 'Done!'}, function(){window.location.href='ManageUser-Manage_Customer.php'});";
+            echo'</script>';
+        }
     }
     ?>
 
