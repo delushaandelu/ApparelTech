649d4eac6ecb6bfb1505d967292ae83cf9ea46cb
Delushaan <delushaan@outlook.com>
2016-09-10 21:34:32 +0530

    delushaan
    stock managerStock.php re modeled

--- stockmanager/Stock_ManageStock.php
+++ stockmanager/Stock_ManageStock.php
@@ -72,122 +72,56 @@
                 <li id ="nav_tab_item_effect"><a href="#">Update Item</a></li>
   			</ul>
             <br><br>
-            
-                
-    
-                 <div class="col-md-4">
-                    <button onclick="location.href = 'Stock_ManageStock.php';" id="btnManageStockAddNew" class="myButton" >Add New</button><br><br>
-                     <button onclick="location.href = 'AddToExisting.php';" id="btnManageStockAddToExisting" class="myButton" >Add To Existing</button>
-
-
-
-
-                 </div>
-
-                 <div class="col-md-8">       
-                	<div class = "upper_panel_add_item">
-                            
+                 
+                	<div class = "upper_panel_add_item" id="add">
                                 <form method="post" name="form" onSubmit="return check();">
                                     <table border="0" >
-                                        <tr></tr>
-                                        <tr>
-                                            
-                                            <td id="table_font"  width="60%"> 
-                                                Category Name   
-                                            </td> 
-                                            
-                                            <td>
-                                                                                                
-
-                                                <select id = "categoryname" name="categoryname" class="add_dropdown_effects">
-                                                    <option value="0"> Select Category Name</option></br>
+                                        
+                                        <tr class="spaceUnder">
+                                            <td id="table_font"  width="20%"> Category Name</td> 
+                                            <td><select id = "categoryname" name="categoryname" class="add_dropdown_effects">
+                                                    <option value="0"> Select Category Name</option>
                                                     <option value="Sewing Machines">Sewing Machines</option>
                                                     <option value="Sewing Machine Spare Parts">Sewing Machine Spare Parts</option>
                                                     <option value="Tools">Tools</option>
-                                                </select><br><br><br>
-                  
+                                                </select>
                                             </td>
-                                                
-                                                
-                                             
+                                        </tr>
+                                        <tr class="spaceUnder">
+                                            <td id="table_font" width="10%">Item Name</td> 
+                                            <td><input type="text"  name="itemname" class="form-control"></td>
+                                        </tr>
+                                       <tr class="spaceUnder">
+                                            <td id="table_font"  width="10%"> Brand</td>
+                                            <td><input type="text"  name="brandname" class="form-control"></td>
                                         </tr>
                                         <tr>
-                                            
-                                            <td id="table_font" width="55%">
-                                                Item Name
-                                            </td> 
-                                            
-                                            <td>
-                                                <input type="text" class="form-control" name="itemname"><br><br>
-                                            </td>
+                                            <td id="table_font" width="10%"> Price</td> 
                                         </tr>
-                                        
-                                       
-                                       <tr>
-                                            
-                                            <td id="table_font"  width="55%">
-                                                Brand
-                                            </td> 
-                                              
-                                            <td>
-                                                <input type="text" class="form-control" name="brandname"><br><br>
-                                            </td>
+                                         <tr class="spaceUnder">
+                                             <td id="table_font" width="10%" align="right"></td> 
+                                             <td><input type="text"  placeholder="Cost"  name="cost" class="form-control" id="exampleInputAmount"  ></td>
                                         </tr>
-                                        
-                                        <tr>
-                                            
-                                            <td id="table_font" width="55%">
-                                                Price
-                                            </td> 
-                                             
-                                            <td>
-                                                <br><br>
-                                            </td>
+                                        <tr class="spaceUnder">
+                                            <td id="table_font" width="10%" align="right" ></td> 
+                                            <td><input type="text"  placeholder="Selling Price" name="sellingprice" class="form-control"></td>
                                         </tr>
-                                        
-                                         <tr>
-                                            
-                                            <td id="table_font" width="55%" align="right">
-                                                Cost                            
-                                            </td> 
-                                             
-                                            <td>
-                                                <input type="text" class="form-control"  name="cost" ><br><br>
-                                            </td>
-                                        </tr>
-                                        
-                                        <tr>
-                                            
-                                            <td id="table_font" width="55%" align="right" >
-                                                Selling Price                
-                                            </td> 
-                                             
-                                            <td>
-                                                <input type="text" class="form-control" name="sellingprice" ><br><br>
-                                            </td>
-                                        </tr>
-                                        
-                                        <tr>
-                                            
-                                            <td id="table_font" width="55%">
-                                                Quantity
-                                            </td> 
-                                             
-                                            <td>
-                                                <input type="text" class="form-control" name="quantity" ><br><br>
-                                            </td>
-                                        </tr>
-                                        
-                                        
+                                        <tr class="spaceUnder">
+                                            <td id="table_font" width="10%"> Quantity</td> 
+                                            <td> <input type="text"  name="quantity" class="form-control"s></td>
+                                        </tr>  
                                     </table> 
-                                 
-                        	</div>
+                            	</div>
+                        <div id="btn">
+                            <input type="submit" class="myButton" id="btnManageStockInsert"  name="btnManageStockInsert" value="Add"  />
+                            <button onclick="location.href = 'Stock_ManageStock.php';" id="btnManageStockAddNew" class="myButton" >Add New</button>
+                            <button onclick="location.href = 'AddToExisting.php';" id="btnManageStockAddToExisting" class="myButton" >Add To Existing</button>
+                        </div>
+               
+                      	<div id="button">
+                            <!-- Dont remove this tag. its refer the margin space  -->
+                        </div>
 
-                            
-                </div>
-                      	<div class = "below_panel_add_item">
-                            <br><br>
-                                 <input type="submit" class="myButton" id="btnManageStockInsert"  name="btnManageStockInsert" value="Add"  /><br><br>
                             	
                          </div>       
                             </form>
@@ -207,19 +141,8 @@
                                     $myItem = new Item();
                                     $myItem -> addItem($itemname, $brand,$sellingprice,$cost, $categoryname,$quantity );
                                 }
-
-                                
                             ?>
-
-
-
-
     </div>
-
-        
-
-    
-   
 </div>
         <script type="text/javascript" src="js/plugins/jquery/jquery.min.js"></script>
         <script type="text/javascript" src="js/plugins/jquery/jquery-ui.min.js"></script>
