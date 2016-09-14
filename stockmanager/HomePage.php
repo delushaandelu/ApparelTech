b368ded98e4872e504218420a1f4caad7ea455e0
Delushaan <delushaan@outlook.com>
2016-09-14 09:04:47 +0530

    Delushaan
    slider maded

--- stockmanager/HomePage.php
+++ stockmanager/HomePage.php
@@ -7,7 +7,10 @@
 
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>                       
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link id="bootstrap-style" href="css/bootstrap.min.css" rel="stylesheet">
+	
+
     </head>
     <body>
         
@@ -42,7 +45,7 @@
             </td>
         </tr>
     </table>
-</div>              
+</div>   
 
         <script type="text/javascript" src="js/plugins/jquery/jquery.min.js"></script>
         <script type="text/javascript" src="js/plugins/jquery/jquery-ui.min.js"></script>
