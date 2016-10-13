77e8a06f5ed8b8475f1d154ce52cee179886862e
Delushaan <delushaan@outlook.com>
2016-10-13 09:29:19 +0530

    Rent add item screen maded

--- customer/newRent.php
+++ customer/newRent.php
@@ -12,19 +12,55 @@ session_start();
     <link rel="stylesheet" href="css/newRent.css" type="text/css">
       <script src="js/sweetalert-dev.js"></script>
     <link rel="stylesheet" href="js/sweetalert.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+   
+    <style>
+
+    .cart-link{width: 100%;text-align: right;display: block;font-size: 22px;}
+    </style>
   </head>
 
   <body class="nav-md">
     
   <?php
     include("../config/customermenu.php");
+    include('dbConfig.php');
   ?>
 
 <!-- page content -->
 <div class="right_col" role="main">
-    <h1 class="hfont">Rent Goods Here!</h1>
+    <h1 class="hfont">Rent Machines Here!</h1>
 
-
+ <h1 class="hfont">All Products</h1>
+    <a href="viewCart.php" class="cart-link" title="View Cart"><i class="glyphicon glyphicon-shopping-cart"></i></a>
+    <div id="products" class="row list-group">
+        <?php
+        //get rows query
+        $query = $db->query("SELECT * FROM item where catagery='Sewing Machines'");
+        if($query->num_rows > 0){ 
+            while($row = $query->fetch_assoc()){
+        ?>
+        <div class="item col-lg-4">
+            <div class="thumbnail">
+                <div class="caption">
+                    <h4 class="list-group-item-heading"><?php echo $row["itemName"]; ?></h4>
+                    <p class="list-group-item-text"><?php echo "<b>Brand : </b>". $row["brand"]; ?></p>
+                    <p class="list-group-item-text"><?php echo "<b>Catagery : </b>". $row["catagery"]; ?></p>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p class="lead"><?php echo 'Rs.'.$row["sellingPrice"].'.00';?></p>
+                        </div>
+                        <div class="col-md-6">
+                            <a class="btn btn-primary" href="cartAction.php?action=addToCart&item_id=<?php echo $row["item_id"]; ?>">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php } }else{ ?>
+        <p>Product(s) not found.....</p>
+        <?php } ?>
+</div>
     
 </div>
 </body>
