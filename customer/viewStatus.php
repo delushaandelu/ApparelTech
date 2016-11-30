f661dc779d9424252761baf6bd2f30efa6910fd6
sachin <sachinhasaranda1@gmail.com>
2016-11-30 18:41:47 +0530

    view status

--- customer/viewStatus.php
+++ customer/viewStatus.php
@@ -14,18 +14,57 @@ session_start();
     <link rel="stylesheet" href="css/viewStatus.css" type="text/css">
   </head>
 
-  <body class="nav-md">
+ <body class="nav-md">
     
   <?php
+    include("dbConfig.php");
     include("../config/customermenu.php");
   ?>
 
 <!-- page content -->
 <div class="right_col" role="main">
-    <h1 class="hfont">Your Delivery Request Status!</h1>
-
-
+    <h1 class="hfont">View Delivery Status</h1>
+   
+    <table class="table table-hover">
+        <tr>
+            <th><center>P/O_ID</center></th>
+            <th><center>ADDRESS</center></th>
+            <th><center>DATE</center></th>
+            <th><center>TELEPHONE</center></th>
+            <th><center>BANK</center></th>
+            <th><center>BRANCH</center></th>
+            <th><center>AMOUNT</center></th>
+            <th><center>STATUS</center></th>
+            <th><center>ACTION</center></th>
+        </tr>
+        
+<?php
     
+    $sql= "select * from deliveryrequest ";
+    $result = $db->query($sql);
+        
+    while($row = $result->fetch_assoc()) {
+                ?>
+                <tr>
+                    <td><center><?php echo $row['poid'] ?></center></td>
+                    <td><center><?php echo $row['address'] ?></center></td>
+                    <td><center><?php echo $row['date'] ?></center></td>
+                    <td><center><?php echo $row['telephone'] ?></center></td>
+                    <td><center><?php echo $row['bank'] ?></center></td>
+                    <td><center><?php echo $row['branch'] ?></center></td>
+                    <td><center><?php echo $row['amount'] ?></center></td>
+                    <td><center>Not Approved</center></td>
+                    
+                    <td class="bt"><center>
+                        
+                        <button type="button" id="delete" class="btn btn-success" onclick="location.href='viewStatus.php'"><i class="fa fa-check-square-o" aria-hidden="true"></i> Delete</button>
+                        
+                        </center></td>
+                </tr>
+                <?php } ?>
+
+        
+    </table>
 </div>
 </body>
 
