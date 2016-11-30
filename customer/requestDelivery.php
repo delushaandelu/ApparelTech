702c70b008b3c757964c6b32e14c139cb99bfe55
kalpa sameera <kalpasameeramadanayake@gmail.com>
2016-11-30 13:52:31 +0530

    request delivery

--- customer/requestDelivery.php
+++ customer/requestDelivery.php
@@ -24,14 +24,39 @@ include("dbConfig.php");
   <?php
     $id = $_GET['p_id'];
     $com = $_SESSION['company'];
-  ?>
+  if (isset($_POST['btn'])){
+        $add = $_POST['add'];
+        $date = $_POST['date'];
+        $tele = $_POST['tele'];
+        $bank = $_POST['bank'];
+        $branch = $_POST['branch'];
+        $vid = $_POST['vid'];
+        $amount = $_POST['amount'];    
+    
+    $sql="INSERT INTO deliveryrequest (poid,company,address,date,telephone,bank,branch,voucher,amount) VALUES ('$id','$com','$add','$date','$tele','$bank','$branch','$vid','$amount')";
+    
+
+if (mysqli_query($db, $sql) === TRUE) {
+       echo'<script>';
+        echo"alert('We will give your account authentication soon! Thank you')";
+        echo'</script>';
+    } else {
+        echo'<script>';
+        echo"alert('Error in Submition')";
+        echo'</script>';
+        
+    }
+
+    mysqli_close($db);
+    }
+    ?>
 
 <!-- page content -->
 <div class="right_col" role="main">
     <h1 class="hfont">Request Your Delivery Here!</h1>
 
     <div id="delivery">
-        <form  method="post" action="requestDelivery.php">
+        <form method="post">
             <table class="table table-hover">
                 <tr>
                     <td>P/O ID</td>
@@ -73,40 +98,11 @@ include("dbConfig.php");
                     <td><input type="text" class="form-control" name="amount"></td>
                 </tr>
                 <tr>
-                    <td colspan="2"><center><input type="submit" class="btn btn-success" id="btn" value="Request Delivery"></center></td>
+                    <td colspan="2"><center><input type="submit" class="btn btn-success" name="btn" value="Request Delivery"></center></td>
                 </tr>
             </table>
         </form>
     </div>
-    <?php 
-    if (isset($_POST['btn'])){
-        $poid = $_POST['poid'];
-        $company = $_POST['company'];
-        $add = $_POST['add'];
-        $date = $_POST['date'];
-        $tele = $_POST['tele'];
-        $bank = $_POST['bank'];
-        $branch = $_POST['branch'];
-        $vid = $_POST['vid'];
-        $amount = $_POST['amount'];    
-    
-    $sql="INSERT INTO deliveryrequest (poid,company,address,date,telephone,bank,branch,voucher,amount) VALUES ('$poid','$company','$add','$date','$tele','$bank','$branch','$vid','$amount')";
-    
-
-if (mysqli_query($db, $sql) === TRUE) {
-        echo'<script>';
-        echo"alert('We will give your account authentication soon! Thank you')";
-        echo'</script>';
-    } else {
-        echo'<script>';
-        echo"alert('Error in Submition')";
-        echo'</script>';
-        
-    }
-
-    mysqli_close($db);
-    }
-    ?>
     
 </div>
 </body>
