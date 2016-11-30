41def7f4559b7c22fe964c3a262ef5a374010061
kalpa sameera <kalpasameeramadanayake@gmail.com>
2016-11-30 15:11:20 +0530

    request delivery massage change

--- customer/requestDelivery.php
+++ customer/requestDelivery.php
@@ -13,6 +13,8 @@ include("dbConfig.php");
     <link href="vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
     <link href="build/css/custom.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/requestDelivery.css" type="text/css">
+       <script src="js/sweetalert-dev.js"></script>
+      <link rel="stylesheet" href="js/sweetalert.css">
   </head>
 
   <body class="nav-md">
@@ -36,16 +38,15 @@ include("dbConfig.php");
     $sql="INSERT INTO deliveryrequest (poid,company,address,date,telephone,bank,branch,voucher,amount) VALUES ('$id','$com','$add','$date','$tele','$bank','$branch','$vid','$amount')";
     
 
-if (mysqli_query($db, $sql) === TRUE) {
-       echo'<script>';
-        echo"alert('We will give your account authentication soon! Thank you')";
-        echo'</script>';
-    } else {
-        echo'<script>';
-        echo"alert('Error in Submition')";
-        echo'</script>';
-        
-    }
+if ($db->query($sql) === TRUE) {
+            echo'<script language ="javascript">';
+                    echo "swal({  title: 'We will give your account authentication soon! Thank you', text: '', type: 'success', confirmButtonText: 'Done!'}, function(){window.location.href='purchaseReport.php'});";
+                echo'</script>';
+        } else {
+             echo'<script language ="javascript">';
+                    echo "swal({  title: 'Error!', text: '', type: 'error', confirmButtonText: 'Done!'}, function(){window.location.href='purchaseReport.php'});";
+                echo'</script>';
+        }
 
     mysqli_close($db);
     }
@@ -72,11 +73,11 @@ if (mysqli_query($db, $sql) === TRUE) {
                 </tr>
                 <tr>
                     <td>Delivery Date</td>
-                    <td><input type="text" class="form-control" name="date"></td>
+                    <td><input type="date" class="form-control" name="date"></td>
                 </tr>
                 <tr>
                     <td>Telephone No for Delivery</td>
-                    <td><input type="text" class="form-control" name="tele"></td>
+                    <td><input type="tel" class="form-control" name="tele"></td>
                 </tr>
                 <tr>
                     <td colspan="2"><h4 align="center">Bank Payment Details</h4></td>
@@ -91,11 +92,11 @@ if (mysqli_query($db, $sql) === TRUE) {
                 </tr>
                 <tr>
                     <td>Deposit Voucher ID</td>
-                    <td><input type="text" class="form-control" name="vid"></td>
+                    <td><input type="number" class="form-control" name="vid"></td>
                 </tr>
                 <tr>
                     <td>Amount</td>
-                    <td><input type="text" class="form-control" name="amount"></td>
+                    <td><input type="number" class="form-control" name="amount"></td>
                 </tr>
                 <tr>
                     <td colspan="2"><center><input type="submit" class="btn btn-success" name="btn" value="Request Delivery"></center></td>
