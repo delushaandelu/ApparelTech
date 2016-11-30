e7afe569ec5c5e38715302cd81fcbe9d6e5c26d6
kalpa sameera <kalpasameeramadanayake@gmail.com>
2016-11-30 16:26:02 +0530

    manage delivery

---
+++ stockmanager/managedelivery.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>        
+        <title>Stock Manager</title>                   
+        <link rel="stylesheet" type="text/css" id="theme" href="css/main.css"/>                        
+    </head>
+    <body>
+        
+        <?php
+            include("../config/stockmgrmenu.php");
+        ?>
+
+                <ul class="breadcrumb">
+                    <h2>Page Heading here!</li></h2>
+                </ul>
+
+        <script type="text/javascript" src="js/plugins/jquery/jquery.min.js"></script>
+        <script type="text/javascript" src="js/plugins/jquery/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="js/plugins/bootstrap/bootstrap.min.js"></script>        
+        <script type="text/javascript" src="js/settings.js"></script>
+        <script type="text/javascript" src="js/plugins.js"></script>        
+        <script type="text/javascript" src="js/actions.js"></script>
+    </body>
+</html>
