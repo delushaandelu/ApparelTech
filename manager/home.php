6101df59997d0fbfca063ce0f41131649e43bdd5
Chamrith <chamrith@gmail.com>
2016-12-06 11:21:23 +0530

    manager home page

--- manager/home.php
+++ manager/home.php
@@ -13,7 +13,41 @@
     <link href="assets/plugins/dataTables/dataTables.bootstrap.css" rel="stylesheet" />
     <link href="assets/css/layout2.css" rel="stylesheet" />
     <link href="css/home.css" rel="stylesheet" type="text/css">
-      
+    <style type="text/css">
+    .bt{
+        padding-left: 10%;
+        padding-top: 15%;
+        width: 230px;
+        height: 150px;
+       
+       
+    }  
+    #dash{
+            margin-top:-38%;
+            margin-left:20%;
+        }
+    #dash1{
+            margin-top:10%;
+            margin-left: 20%;
+            width: 1080px;
+        }
+    .bt a {
+       text-align: center;
+        text-decoration: none;
+        color: aliceblue;
+        font-size: 15px;
+    }
+        .bt a:hover{
+           
+            color: aliceblue;
+            text-decoration: none;
+        }
+        .bt i{
+            margin-top: 15% 
+          margin-left:60%;
+        }
+</style>
+    
     
 </head>
 
@@ -27,235 +61,43 @@
 <div id="head">
     
 </div>
-    <div id="label">
-            <img src="images/mentor-Icon1.png">
-     <div id ="text">
-        <div class="col-lg-4 col-sm-4 follow-info" id="font">
-                                <p>" Our attitude towards others determines their attitude towards us " </p>
-                                <p>priyantha@gmail.com</p>
-								<p><i class="fa fa-twitter">PriyanthaEnt</i></p>
-                                <h6>
-                                    <span><i class="icon_calendar"></i>
-                                    <script language="javascript">
-                                     var today = new Date();
-                                     document.write(today);
-                                     </script></span>
-                                    <span><i class="icon_pin_alt"></i></span>
-                                </h6>
-                            </div>
-                            <div class="col-lg-2 col-sm-6 follow-info weather-category" id="font">
-                                      <ul>
-                                          <li class="active">
-                                              
-                                              <i class="fa fa-comments fa-2x"> </i><br>
-											  
-											 One painful duty fulfilled makes the next plainer and easier.
-                                          </li>
-										   
-                                      </ul>
-                            </div>
-							<div class="col-lg-2 col-sm-6 follow-info weather-category" id="font">
-                                      <ul>
-                                          <li class="active">
-                                              
-                                              <i class="fa fa-bell fa-2x"> </i><br>
-											  
-											  Human happiness and moral duty are inseparably connected.
-                                          </li>
-										   
-                                      </ul>
-                            </div>
-							<div class="col-lg-2 col-sm-6 follow-info weather-category" id="font">
-                                      <ul>
-                                          <li class="active">
-                                              
-                                              <i class="fa fa-tachometer fa-2x"> </i><br>
-											  
-											  It is the first responsibility of every citizen to question authority.
-                                          </li>
-										   
-                                      </ul>
-                            </div>
-            </div>
-        </div>
+    
     </div>
     
     <!-- links -->
     <div id="dash">
                 <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-warning">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-university fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div>Manage Supplier!</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="supplier-manage_supplier.php">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
+                    
+                    <div class="bt" style="background-color:#6495ED"><a href="supplier-manage_supplier.php">Manage Supplier</a><i class="fa fa-arrow-circle-right"></i> <i class="fa fa-university fa-5x"></i></div>
+                    
                 </div>
                 <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-success">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-stack-overflow fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div>View Stock!</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="Stock-View_Stock.php">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
+                    <div class="bt" style="background-color:#008B8B"><a href="Stock-View_Stock.php">View Stock</a><i class="fa fa-arrow-circle-right"></i>  <i class="fa fa-stack-overflow fa-5x"></i></div>
                 </div>
                 <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-info">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-user fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div>Manage Customer!</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="ManageUser-Manage_Customer.php">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
+                    <div class="bt" style="background-color:#8FBC8F"><a href="ManageUser-Manage_Customer.php">Manage Customer</a><i class="fa fa-arrow-circle-right"></i>  <i class="fa fa-user fa-5x"></i></div>
                 </div>
                 <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-danger">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-users fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div>View Customer!</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="ManageUser-View_Customer.php">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
+                    <div class="bt" style="background-color:#DAA520"><a href="ManageUser-View_Customer.php">View Customer</a><i class="fa fa-arrow-circle-right"></i>  <i class="fa fa-users fa-5x"></i></div>
                 </div>
-                </br>
+    </div>
+    </br></br></br>
+
+                <div id="dash1">
                 <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-success">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-user-plus fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div>Add User!</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="ManageUser-Manage_Stock_Manager.php">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
+                     <div class="bt" style="background-color:#CD853F"><a href="ManageUser-Manage_Stock_Manager.php">Add user</a><i class="fa fa-arrow-circle-right"></i><i class="fa fa-user-plus fa-5x"></i></div>
                 </div>
                 <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-danger">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-quote-left fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div>Manage Quotation!</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="Quotation-Manage_Quotation.php">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
+                    <div class="bt" style="background-color:#A9A9A9"><a href="Quotation-Manage_Quotation.php">Manage Quotation</a><i class="fa fa-arrow-circle-right"></i><i class="fa fa-quote-left fa-5x"></i></div>
                 </div>
                 <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-warning">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-cloud-upload fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div>Backup!</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="backup.php">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
+                    <div class="bt" style="background-color:#A52A2A"><a href="backup.php">Backup</a><i class="fa fa-arrow-circle-right"></i><i class="fa fa-cloud-upload fa-5x"></i></div>
                 </div>
                 <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-info">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-unlock-alt fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div>Change Password!</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="Settings-Change_password.php">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
+                    <div class="bt" style="background-color:#DEB887"><a href="Settings-Change_password.php">Change Password</a><i class="fa fa-arrow-circle-right"></i> <i class="fa fa-unlock-alt fa-5x"></i></div>
                 </div>
             </div>
-                </div>
-            </div>
-           
-        </div>
+        
                         
     <div class="row">
 
