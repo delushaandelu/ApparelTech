b42da2c2b07dc40cd1ea981e751fb1ddc2adddfb
Delushaan <delushaan@outlook.com>
2017-01-15 13:41:29 +0530

    payment completed

--- customer/payment.php
+++ customer/payment.php
@@ -12,7 +12,7 @@ $paypal_id = 'info@delushaan.com'; //Business Email
 <title>Products - Delushaan</title>
 </head>
 <body>
-	
+
     <form action="<?php echo $paypal_url; ?>" method="post">
 
         <!-- Identify your business so that you can collect the payments. -->
@@ -21,18 +21,34 @@ $paypal_id = 'info@delushaan.com'; //Business Email
         <!-- Specify a Buy Now button. -->
         <input type="hidden" name="cmd" value="_xclick">
         
-        <!-- Specify details about the item that buyers will purchase. -->
-        <input type="hidden" name="item_name" value="Delushaan">
-        <input type="hidden" name="item_number" value="Item Number">
-        <input type="hidden" name="amount" value="<?php  echo $_SESSION['amount'];?>">
-        <input type="hidden" name="currency_code" value="USD">
+        <?php
+            include('dbConfig.php');
+            $id = $_GET['p_id'];
+            $c_id = $_SESSION['csid'];
+            $name = $_SESSION['fname'];
+            $sql = "SELECT * FROM rentorders where id=$id ";
+            $result = $db->query($sql);
+
+            while($row = $result->fetch_assoc()) {
+                $order = $row['id'];
+                $amount = $row['total_price'];
         
+                ?>              
+
+        <!-- Specify details about the item that buyers will purchase. -->
+        <input type="hidden" name="item_name" value="<?php echo 'C_ID -'. $c_id.'-'.$name ?>">
+        <input type="hidden" name="item_number" value="<?php echo 'Rent Order No'.$order?>">
+        <input type="hidden" name="amount" value="<?php echo $amount ?>">
+        <input type="hidden" name="currency_code" value="USD">
+        <?php
+            }
+        ?>
         <!-- Specify URLs -->
         <input type='hidden' name='cancel_return' value='http://localhost/paypal_integration_php/cancel.php'>
 		<input type='hidden' name='return' value='http://localhost/paypal_integration_php/success.php'>
-
         
         <!-- Display the payment button. -->
+        <div 
         <input type="image" name="submit" border="0"
         src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" alt="PayPal - The safer, easier way to pay online">
         <img alt="" border="0" width="1" height="1" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" >
--- customer/rentReport.php
+++ customer/rentReport.php
@@ -23,7 +23,44 @@ session_start();
 <!-- page content -->
 <div class="right_col" role="main">
     <h1>Your Rent Report!</h1>
-Delushaan
+<table class='table table-hover'  >
+            <tr>
+                <th><b><h5 align='center'>Rent Order ID </h5></b></th>
+                <th><b><h5 align='center'>Total Price </h5></b></th>
+                <th><b><h5 align='center'>Purchase Order Date </h5></b></th>
+                <th><b><h5 align='center'>Payment </h5></b></th>
+                <th><b><h5 align='center'>Amount </h5></b></th>
+                <th><b><h5 align='center'>Action </h5></b></th>
+            </tr>
+
+            <?php
+
+            include('dbConfig.php');
+            $c_id = $_SESSION['csid'];
+            $sql = "SELECT * FROM rentorders where customer_id=$c_id ";
+            $result = $db->query($sql);
+
+            while($row = $result->fetch_assoc()) {
+                ?>
+                <tr>
+                    <td><center><?php echo $row['id'] ?></center></td>
+                    <td><center><?php echo $row['total_price']?></center></td>
+                    <td><center><?php echo $row['created'] ?></center></td>
+                    <td><center><p class="bg-success"><?php echo $row['payment'] ?></p></center></td>
+                    <td><center><p class="bg-primary"><?php echo $row['amount'] ?></p></center></td>
+                    <td class="bt"><center>
+                        
+                        <button type="button" id="view" class="btn btn-primary" onclick="location.href='purchaseitem.php?p_id=<?php echo $row['id'] ?>'"><i class="fa fa-check-square-o" aria-hidden="true"></i> View</button>
+                        
+                        <button type="button" id="delivery"  class="btn btn-success" onclick="location.href='payment.php?p_id=<?php echo $row['id'] ?>'"><i class="fa fa-cc-paypal" aria-hidden="true"></i> Pay</button>
+                        
+                        <button type="button" id="invoice"  class="btn btn-danger" onclick="location.href='invoice.php?p_id=<?php echo $row['id'] ?>'"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> P/O</button>
+                        
+                        </center></td>
+                </tr>
+                <?php } ?>
+            </table>
+     
 </div>
 </body>
 
